40b6fdf5448a0ff5124d0588e96a7caa067418c3
Alan Ramos <alan.ramos@jarsa.com.mx>
2015-06-18 20:07:04 -0500

    [ADD]: Add module description and icon

--- __openerp__.py
+++ __openerp__.py
@@ -1,38 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "zion",
-
-    'summary': """Manage trainings""",
-
-    'description': """
-        Open Academy module for managing trainings:
-            - training courses
-            - training sessions
-            - attendees registration
-    """,
-
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'name': 'Zion Reports',
+    'summary': """Special reports""",
+    'author': 'Jarsa Sistemas, S.A. de C.V.',
+    'website': 'http://www.jarsa.com.mx',
+    'category': 'Reports',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'depends': ['website_quote'],
     'data': [
-        # 'security/ir.model.access.csv',
-        #'templates.xml',
-        'zion_report.xml',
-        #'data/report_paperformat.xml',
-        'reports/zion_proposal.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        #'demo.xml',
-    ],
+            # 'security/ir.model.access.csv',
+            #'templates.xml',
+            'zion_report.xml',
+            #'data/report_paperformat.xml',
+            'reports/zion_proposal.xml',
+            ],
+    'demo': [],
+    #'license': 'AGPLv3'
 }
